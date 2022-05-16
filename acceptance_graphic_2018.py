aa6f2cef0ff86a9bd256db5dbf6a2862c2140fc6
estading <82887316+estading@users.noreply.github.com>
2022-05-15 23:41:04 -0500

    All college acceptance graphics uploaded
    If there appears to be a bar missing in the graphic, it is because there was no data provided by the clearing house. This is because less than 10 students were in the total makeup of a group. This should have a note in the webpage as well.

---
+++ acceptance_graphic_2018.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import plotly.graph_objects as go
+import numpy as np
+acceptance_df = pd.read_csv('CollegeAcceptanceScraped.csv', dtype=str)
+acceptance_df = acceptance_df.replace('*', 0)
+acceptance_df['PercentAcceptance'] = pd.to_numeric(acceptance_df['PercentAcceptance'].str.strip('%'))
+acceptance_df = acceptance_df.replace(np.nan, 0)
+acceptance_df = acceptance_df.astype({'NumberAcceptance': 'int64', 'ClassYear': 'int64'})
+
+acceptance2018 = acceptance_df.groupby(acceptance_df.ClassYear).get_group(2018)
+aa_acceptance_2018 =acceptance2018.groupby(acceptance2018.SchoolName).get_group('Black or African American')
+asian_acceptance_2018 =acceptance2018.groupby(acceptance2018.SchoolName).get_group('Asian')
+native_acceptance_2018 =acceptance2018.groupby(acceptance2018.SchoolName).get_group('American Indian or Native Alaskan')
+white_acceptance_2018 =acceptance2018.groupby(acceptance2018.SchoolName).get_group('White')
+pacific_acceptance_2018 =acceptance2018.groupby(acceptance2018.SchoolName).get_group('Native Hawaiian or Pacific Islander')
+two_acceptance_2018 =acceptance2018.groupby(acceptance2018.SchoolName).get_group('Two or more races')
+latino_acceptance_2018 =acceptance2018.groupby(acceptance2018.SchoolName).get_group('Hispanic/Latino')
+
+fig2018 = go.Figure(data = [go.Bar(name='Black or African American', 
+x = aa_acceptance_2018['SchoolName'], 
+y = aa_acceptance_2018['PercentAcceptance']), 
+go.Bar(name='Asian', 
+x = asian_acceptance_2018['SchoolName'], 
+y = asian_acceptance_2018['PercentAcceptance']),
+go.Bar(name='American Indian or Native Alaskan', 
+x = native_acceptance_2018['SchoolName'], 
+y = native_acceptance_2018['PercentAcceptance']), 
+go.Bar(name='White', 
+x = white_acceptance_2018['SchoolName'], 
+y = white_acceptance_2018['PercentAcceptance']),
+go.Bar(name='Native Hawaiian or Pacific Islander', 
+x = pacific_acceptance_2018['SchoolName'], 
+y = pacific_acceptance_2018['PercentAcceptance']),
+go.Bar(name='Two or more races', 
+x = two_acceptance_2018['SchoolName'], 
+y = two_acceptance_2018['PercentAcceptance']),
+go.Bar(name='Hispanic/Latino', 
+x = latino_acceptance_2018['SchoolName'], 
+y = latino_acceptance_2018['PercentAcceptance'])])
+fig2016.update_layout(barmode='group', yaxis_range=[0,100], title='2016 College Attendance by Percent', yaxis_title='Percent Attendance')
+fig2016.show()
